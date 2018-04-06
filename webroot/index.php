e7e5c52d16687cf4f2af514e5aedb1e657d4ef5d
Tom Flitterman <tom.flitterman@hotmail.fr>
2018-04-06 14:50:48 +0200

    Add base

---
+++ webroot/index.php
@@ -0,0 +1,11 @@
+<?php
+include('../config.php');
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <link rel="stylesheet" href="pages/style/style.css">
+    </head>
+</html>
