4ff89100fa8078883c11a8a8ff82832fb1bc47b3
Tom Flitterman <tom.flitterman@hotmail.fr>
2018-04-11 16:34:12 +0200

    Update

--- webroot/index.php
+++ webroot/index.php
@@ -1,5 +1,5 @@
 <?php
-include('../config.php');
+    include('../config.php');
 ?>
 
 <!DOCTYPE html>
@@ -7,10 +7,16 @@ include('../config.php');
     <head>
         <meta charset="utf-8" />
         <link rel="stylesheet" href="pages/style/style.css">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
         <title>Lebowski - <?= $handler->pageTitle(); ?></title>
     </head>
 
     <body>
-        <?php include($page); ?>
+        <div class="main_wrapper">
+            <?php
+                include('pages/src/header.php');
+                include($page);
+            ?>
+        </div>
     </body>
 </html>
