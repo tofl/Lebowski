e8258aac04228728534300278e0405b53df0f782
Tom Flitterman <tom.flitterman@hotmail.fr>
2018-04-22 19:36:47 +0200

    Ajout du classement des équipes

--- webroot/pages/teams_rank.php
+++ webroot/pages/teams_rank.php
@@ -1,10 +1,32 @@
 <h1 class="login_title">Classement global des équipes</h1>
 
-<?php
-$getTeams = new User();
-$teams = $getTeams->getTeams();
+<table class="table">
+    <thead>
+    <tr>
+        <th scope="col">🏆</th>
+        <th scope="col">Nom</th>
+        <th scope="col">Motto</th>
+        <th scope="col">Score total</th>
+    </tr>
+    </thead>
 
-echo '<pre>';
-var_dump($teams);
-echo '</pre>';
-?>
+    <tbody>
+        <?php
+            $i = 1;
+            $getTeams = new User();
+            $teams = $getTeams->getTeams();
+
+            foreach ($teams as $team) {
+        ?>
+                <tr>
+                    <th><?= $i; ?></th>
+                    <td><?= $team['name']; ?></td>
+                    <td><?= $team['phrase']; ?></td>
+                    <td><?= $team['score_total']; ?></td>
+                </tr>
+        <?php
+                $i++;
+            }
+        ?>
+    </tbody>
+</table>
